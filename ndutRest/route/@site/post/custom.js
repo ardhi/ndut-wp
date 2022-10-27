809bc91ce7063c3630f83d0d40dc51bec41a2f3d
Ardhi Lukianto <ardhi@lukianto.com>
2022-10-27 21:21:36 +0700

    First commit

---
+++ ndutRest/route/@site/post/custom.js
@@ -0,0 +1,14 @@
+module.exports = async function () {
+  const { _, getNdutConfig } = this.ndut.helper
+  const cfg = getNdutConfig('wp')
+  let remote = _.get(cfg, 'restProxy.remote')
+  if (remote) {
+    if (_.isPlainObject(remote)) remote = [remote]
+  }
+  return {
+    type: 'ndutRestProxy',
+    swaggerTags: 'WordPress',
+    remoteTag: 'post',
+    remote
+  }
+}
